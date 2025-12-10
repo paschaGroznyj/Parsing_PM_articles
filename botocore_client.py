9022a0958d3b0bee05da38da469ba5d5dfc734ec
paschaGroznyj <pascha.soldatkin2015@yandex.ru>
2025-12-10 12:11:25 +0400

    update

--- botocore_client.py
+++ botocore_client.py
@@ -4,18 +4,22 @@ from contextlib import AsyncExitStack
 from aiobotocore.client import AioBaseClient
 from aiobotocore.session import AioSession
 from botocore.client import Config
-from environs import env
+from dotenv import load_dotenv
+import os
+import asyncio
 
+load_dotenv()
 
 params = {
     "service_name": "s3",
-    "aws_access_key_id": env("OBS_ACCESS_KEY"),
-    "aws_secret_access_key": env("OBS_SECRET_KEY"),
-    "region_name": env("OBS_REGION"),
-    "endpoint_url": env("OBS_ENDPOINT"),
+    "aws_access_key_id": os.getenv("OBS_ACCESS_KEY"),
+    "aws_secret_access_key": os.getenv("OBS_SECRET_KEY"),
+    "region_name": os.getenv("OBS_REGION"),
+    "endpoint_url": os.getenv("OBS_ENDPOINT"),
     "config": Config(s3={"addressing_style": "virtual"})
 }
 
+print(params)
 
 async def create_async_client(session: AioSession, exit_stack: AsyncExitStack):
     context_manager = session.create_client(**params)
@@ -28,3 +32,37 @@ async def get_async_client() -> AsyncGenerator[AioBaseClient, None]:
     async with AsyncExitStack() as exit_stack:
         client = await create_async_client(session, exit_stack)
         yield client
+
+
+# ---------------------------
+#      Тестовый сценарий
+# ---------------------------
+async def put_object():
+    session = AioSession()
+    async with AsyncExitStack() as stack:
+        client = await stack.enter_async_context(
+            session.create_client(**params)
+        )
+
+        bucket = os.getenv("OBS_BUCKET") # cds-bucket
+        key = "test_upload.txt"
+
+        body = b"hello world"
+
+        print("Uploading...")
+
+        await client.put_object(
+            Bucket=bucket,
+            Key=key,
+            Body=body,
+            ContentType="text/plain"
+        )
+
+        print("Downloading...")
+        response = await client.get_object(Bucket=bucket, Key=key)
+
+        data = await response["Body"].read()
+        print("Downloaded:", data)
+
+if __name__ == "__main__":
+    asyncio.run(put_object())
